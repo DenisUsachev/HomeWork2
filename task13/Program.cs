65cc20aed93ef093a74a1a69c1c77b5f38abe9ac
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-04 16:49:44 +0500

    solved the 13th problem

---
+++ task13/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Напишиште число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number > 99)
+    Console.WriteLine(number.ToString()[2]);
+else Console.WriteLine("Третьей цифры нет");
