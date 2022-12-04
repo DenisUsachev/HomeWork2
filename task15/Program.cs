630bf0e215327408e55fb842111897b99f18c9c6
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-04 16:23:39 +0500

    solved the 15th problem

---
+++ task15/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите день недели(число)");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7)
+    Console.WriteLine("Сегодня выходной день!");
+else Console.WriteLine("Сегодня рабочий день!");
